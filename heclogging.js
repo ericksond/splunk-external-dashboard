2984d013819d8be229dce0b6f64f1a156359f2c1
Erickson Delgado <ericksond@gmail.com>
2016-08-11 08:33:31 -0400

    HTTP Event Collector Stream for Bunyan

---
+++ heclogging.js
@@ -0,0 +1,26 @@
+var splunkBunyan = require("splunk-bunyan-logger");
+var bunyan = require("bunyan");
+
+var config = {
+  token: "13EA3875-2879-4F44-AF0A-3ED336CB5BCA",
+  url: "https://localhost:8088"
+};
+var splunkStream = splunkBunyan.createStream(config);
+
+splunkStream.on("error", function(err, context) {
+  console.log("Error", err, "Context", context)
+});
+
+var Logger = bunyan.createLogger({
+    name: "logger",
+    streams: [
+        splunkStream
+    ]
+});
+
+var payload = {
+  event: "logging event #" + Math.random()
+};
+
+console.log("Sending payload", payload);
+Logger.info(payload, "Posted successfully.")
